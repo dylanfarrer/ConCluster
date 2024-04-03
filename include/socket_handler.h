e31505a910d3384e46645a7a93225e8546eea1e9
Dylan Farrer <dylan.farrer.2019@live.rhul.ac.uk>
2024-04-03 17:06:15 +0100

    initial population of socket_handler. TODO -> error checking and listen function

--- include/socket_handler.h
+++ include/socket_handler.h
@@ -1,12 +1,9 @@
 #ifndef SOCKET_HANDLER_H
 #define SOCKET_HANDLER_H
 
-#define DEFAULT_PORT 8080
-#define BUFFER_SIZE 1024
-
 typedef void (*FunctionPtr)();
 
-int create_listener_socket();
+int create_listener_socket(int port);
 
 int listen_on_socket(int listener,
            char** endpoints,
@@ -16,6 +13,9 @@ int listen_on_socket(int listener,
 
 int create_sender_socket();
 
-char* send_and_recieve_on_socket(int sender, const char* message);
+char* send_and_recieve_on_socket(int sender,
+                                 const char* ip,
+                                 int port,
+                                 const char* message);
 
 #endif
--- src/socket_handler.c
+++ src/socket_handler.c
@@ -1,9 +1,40 @@
 #include "../include/socket_handler.h"
+
+// TODO check if we need all of these
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netdb.h>
 
-int create_listener_socket() {
-    return 0;
+#define DEFAULT_PORT 8080
+#define BUFFER_SIZE 1024
+
+// TODO: add error checking and early failure here
+int create_listener_socket(int port) {
+    int listenfd = 0;
+	struct sockaddr_in serv_addr;
+
+	char sendBuff[BUFFER_SIZE];
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(sendBuff, '0', sizeof(sendBuff));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(5000);
+
+	bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+	listen(listenfd, 10);
+
+    return listenfd;
 }
 
 int listen_on_socket(int listener_socket,
@@ -15,9 +46,61 @@ int listen_on_socket(int listener_socket,
 }
 
 int create_sender_socket() {
-    return 0;
+    int sockfd = 0;
+	char recvBuff[BUFFER_SIZE];
+	memset(recvBuff, '0', sizeof(recvBuff));
+
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Error : Could not create socket \n");
+		return 1;
+	}
+    return sockfd;
 }
 
-char* send_and_receive_on_socket(int sender_socket, const char* message) {
-    return NULL;
+char* send_and_receive_on_socket(int sender_socket, const char* ip, int port, const char* message) {
+    // Create socket address structure for the server
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
+
+    // Connect to the server
+    if (connect(sender_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Connect failed");
+        return NULL;
+    }
+
+    // Send message to the server
+    if (send(sender_socket, message, strlen(message), 0) < 0) {
+        printf("Send failed");
+        return NULL;
+    }
+
+    // Receive response from the server
+    char buffer[BUFFER_SIZE];
+    int bytes_received = recv(sender_socket, buffer, BUFFER_SIZE, 0);
+    if (bytes_received < 0) {
+        printf("Receive failed");
+        return NULL;
+    }
+
+    // Null-terminate the received data
+    buffer[bytes_received] = '\0';
+
+    // Allocate memory for the received message
+    char *received_message = malloc(strlen(buffer) + 1);
+    if (received_message == NULL) {
+        printf("Memory allocation failed");
+        return NULL;
+    }
+
+    // Copy the received data to the allocated memory
+    strcpy(received_message, buffer);
+
+    // Close the socket
+    close(sender_socket);
+
+    // Return the received message
+    return received_message;
 }
