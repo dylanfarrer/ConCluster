00dd5c808282185386c97f208d40e9832949cf0c
Dylan Farrer <dylan.farrer.2019@live.rhul.ac.uk>
2024-05-29 13:38:57 +0100

    generated necessary files, filled out skeleton for cluster_manager header

---
+++ examples/simple-tcp/include/cluster_manager.h
@@ -0,0 +1,45 @@
+#ifndef CLUSTER_MANAGER_H
+#define CLUSTER_MANAGER_H
+
+#include "../../utils/include/socker_handler.h"
+#include "../../utils/include/thread_handler.h"
+
+#include <concluster/cluster.h>
+#include <stdatomic.h>
+
+/**
+ * @brief struct that wraps arounf ccon_cluster, providing an additional
+ * message_count metric, that will log the number of messages sent between
+ * nodes.
+ */
+typedef struct {
+    ccon_cluster cluster;
+    _Atomic(int) message_count;
+} cluster;
+
+/**
+ * @brief create a cluster of usecase specific nodes, with a configurable size.
+ * 
+ * @param node_count 
+ * @return cluster* 
+ */
+cluster* generate_cluster(int node_count);
+
+/**
+ * @brief start the nodes talking with each other for a given time frame.
+ * 
+ * @param ms_time 
+ * @return int 
+ */
+int perform_chatter_event(int ms_time);
+
+/**
+ * @brief delete a cluster struct.
+ * 
+ * @param cluster_to_delete 
+ * @return true 
+ * @return false 
+ */
+bool delete_cluster(cluster** cluster_to_delete);
+
+#endif
